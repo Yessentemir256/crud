f6cf9c4a6a156cd5b58045582b777e295fd22871
Yessentemir <yessentemir.utepov@gmail.com>
2024-12-22 20:15:18 +0500

    corrected requests

--- requests.sql
+++ requests.sql
@@ -10,14 +10,15 @@ SELECT * FROM customers;
 
 SELECT * FROM customers WHERE id = 1;
 
-CREATE TABLE IF NOT EXISTS managers
-(
-	id BIGSERIAL PRIMARY KEY,
-	name TEXT    NOT NULL,
-	salary INTEGER NOT NULL CHECK ( salary > 0),
-	plan INTEGER NOT NULL CHECK ( plan > 0 ),
-	boss_id BIGINT REFERENCES managers,
-	department TEXT,
-	active BOOLEAN NOT NULL DEFAULT TRUE,
-	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS managers (
+	 id BIGSERIAL PRIMARY KEY,
+	 name TEXT    NOT NULL,
+	 salary       INTEGER NOT NULL CHECK ( salary > 0),
+	 plan         INTEGER NOT NULL CHECK ( plan > 0 ),
+	 boss_id      BIGINT REFERENCES managers,
+	 department   TEXT,
+	 active       BOOLEAN NOT NULL DEFAULT TRUE,
+	 created      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	 login        TEXT,
+	 password     TEXT,
 );
